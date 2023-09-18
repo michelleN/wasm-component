f48c6f8b20834de6bb5fb235d8e55299bdcdcac1
Michelle Dhanani <michelle@fermyon.com>
2023-09-18 12:14:15 -0400

    add wasm-tools cli with inspect subcommand
    + The inspect commands takes 1 argument: path
    to .wasm file. It runs wasm-tools component wit
    to get the wit file, then saves file to filesystem
    (TODO: replace with temp file). Then uses that file
    to run wit-bindgen for rust bindings.
    
    Signed-off-by: Michelle Dhanani <michelle@fermyon.com>

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use clap::{Parser, Subcommand};
+use std::path;
+use std::process::Command;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Inspect the wit file of a component
+    Inspect { path: String },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Inspect { path }) => {
+            inspect(path).unwrap();
+        }
+        None => {}
+    }
+
+    // Continued program logic goes here...
+}
+
+fn inspect(path: &str) -> Result<(), std::io::Error> {
+    let p = path::Path::new(&path);
+
+    // Check that file exists
+    std::fs::File::open(p).unwrap();
+
+    Command::new("wasm-tools")
+        .args(["component", "wit", path, "--output=guest.wit"])
+        .spawn()
+        .expect("Failed to run the binary");
+
+    Command::new("wit-bindgen")
+        .args(["rust", "guest.wit", "--output=src/wit.rs"])
+        .spawn()
+        .expect("Failed to run the binary");
+
+    Ok(())
+}
